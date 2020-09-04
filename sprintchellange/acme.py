82e632868e5eb77dfd7f76af70c0b005e6d7888f
shemaaali <shemaali8787@gmail.com>
2020-09-03 22:08:37 -0500

    Added more changes to acme files

---
+++ sprintchellange/acme.py
@@ -0,0 +1,100 @@
+# Some import library about the OOP
+import random as rm
+
+
+# Part 1: define a class
+class Product:
+    """
+    Create a class called a Product
+    """
+
+    def __init__(self,
+                 name,
+                 price=10,
+                 weight=20,
+                 flammability=0.5,
+                 identifier=rm.randrange(1000000, 9999999)):
+        """
+        Define a constructor method to include some fields
+        """               
+
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
+
+    # Part 2: define an object
+    def stealability(self):
+        """
+        Define a function is called stealabilit
+        To do specific condition and return
+        """
+
+        product  = self.price/self.weight
+
+        if product  < 0.5:
+            return("Not so stealable...")
+
+        elif (product >= 0.5) & (product < 1):
+            return("Kinda stealable.")
+
+        else:
+            return("Very stealable!")
+
+    def explode(self):
+        """
+        Define a function is called an explode 
+
+        To calculates the flammability multiple by the weight & return the value for each
+        """
+        exp = self.weight*self.flammability
+
+        if exp < 10:
+            return("...fizzle.")
+
+        elif (exp >= 10) & (exp < 50):
+            return("...boom!")
+
+        else:
+            return("...BABOOM!!")
+
+# Part 3: define subclass
+class BoxingGlove(Product):
+    """
+    Proper Inheritance subclass from the parent(BoxingGlove)
+    """
+
+    def __init__(self,
+                 name,
+                 price=10,
+                 weight=10,
+                 flammability=0.5,
+                 identifier=rm.randint(1000000, 9999999)):
+
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
+
+        super()
+    # Override function
+    def explode(self):
+       
+        return("...it's a glove")
+
+    def punch(self):
+        """
+        Add another subclass called a punch
+        which is returned "That tickles."
+        """
+
+        if self.weight < 5:
+            return('That tickles.')
+
+        elif (self.weight >= 5) & (self.weight < 15):
+            return('Hey that hurt!')
+
+        else:
+            return('OUCH!')
---
+++ sprintchellange/acme_report.py
@@ -0,0 +1,40 @@
+from acme import Product
+import random as rm
+
+
+# Part 4 - Class Report
+def generate_products(x=30):
+    """
+    Generate number of products represents as x 
+    Randomly return the number of products into list.
+    """
+    adjectives = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
+    nouns = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
+    name = [rm.choice(adjectives) + " " + rm.choice(nouns) for i in range(x)]
+    price = [rm.choice([i for i in range(5, 100)]) for i in range(x)]
+    weight = [rm.choice([i for i in range(5, 100)]) for i in range(x)]
+    flammability = [round(rm.uniform(0, 2.5), 2) for i in range(x)]
+    products = [[name[i], price[i], weight[i],
+                 flammability[i]] for i in range(x)]
+    return products
+
+
+def inventory_report(products):
+    """
+    Report and calculate Number of unique product names into  the product list
+    Average(mean)price, weight, and flammability as a list for products
+    """
+    nunique = len(set(i[0] for i in products))
+    avgprice = sum(i[1] for i in products) / len(products)
+    avgweight = sum(i[2] for i in products) / len(products)
+    avgflammability = sum(i[3] for i in products) / len(products)
+    
+    print("Acme Corporation Official Inventory Report:")
+    print("Unique Product Names:", nunique)
+    print("Average Or Mean Price:", avgprice)
+    print("Average Or Mean Weight:", avgweight)
+    print("Average Or Mean Flammability", avgflammability)
+
+
+if __name__ == '__main__':
+    inventory_report(generate_products())
